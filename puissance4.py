22472a32f044d64cdbf015d6638be81ffef707b1
Anthony Manikhouth <Anthony@macbook-pro-anthony.home>
2019-04-16 19:22:14 +0200

    showing tableau with possibility to add piece

--- puissance4.py
+++ puissance4.py
@@ -12,8 +12,16 @@ def currentHauteur(tableau,column): # retourne la ligne (hauteur) du jeu dans un
 		if tableau[i][column] == 0:
 			return i
 
-def isTableauCase(tableau,column):
-	pass
+def isTableauCase(tableau,ligne,column):
+	return tableau[ligne][column] == 0
+
+def dropPiece(tableau, ligne, column, playerPiece):
+	tableau[ligne][column] = playerPiece # 1 
+
+def add(tableau, column, playerPiece):
+	ligne = currentHauteur(tableau,column)
+	if isTableauCase(tableau, ligne ,column):
+		dropPiece(tableau,ligne,column, playerPiece)
 
 tableau = tableau()
 
@@ -24,6 +32,9 @@ while not finJeu:
 	if turn == 0:
 		play = int(input("PLAYER 1: Please enter column: "))
 		turn += 1
+		add(tableau,play,1)
 	else:
 		play = int(input("PLAYER 2: Please enter column: "))
 		turn = 0
+		add(tableau,play,2)
+	print(tableau)
