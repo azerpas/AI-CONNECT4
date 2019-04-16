b4c500b9f909a2e173a2fa9501569003923bc673
Anthony Manikhouth <Anthony@macbook-pro-anthony.home>
2019-04-16 19:13:27 +0200

    currentHauteur

--- puissance4.py
+++ puissance4.py
@@ -1,18 +1,29 @@
 import numpy as np
 
-def board():
-	board = np.zeros((6,7))
-	return board
+LIGNES = 6
+COLONNES = 7
 
-board = board()
+def tableau():
+	tableau = np.zeros((LIGNES,COLONNES))
+	return tableau
+
+def currentHauteur(tableau,column): # retourne la ligne (hauteur) du jeu dans une colonne
+	for i in range(LIGNES):
+		if tableau[i][column] == 0:
+			return i
+
+def isTableauCase(tableau,column):
+	pass
+
+tableau = tableau()
 
 finJeu = False
 turn = 0
 
 while not finJeu:
 	if turn == 0:
-		play = input("PLAYER 1: Please enter column: ")
+		play = int(input("PLAYER 1: Please enter column: "))
 		turn += 1
 	else:
-		play = input("PLAYER 2: Please enter column: ")
+		play = int(input("PLAYER 2: Please enter column: "))
 		turn = 0
