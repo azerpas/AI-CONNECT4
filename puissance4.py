18708f498758766fe368213d2dce4fafec5ad409
Anthony Manikhouth <Anthony@macbook-pro-anthony.home>
2019-04-17 20:36:31 +0200

    Finished and added color code

--- puissance4.py
+++ puissance4.py
@@ -1,4 +1,7 @@
 import numpy as np
+import sys
+import math
+from colors import colors
 
 LIGNES = 6
 COLONNES = 7
@@ -7,34 +10,80 @@ def tableau():
 	tableau = np.zeros((LIGNES,COLONNES))
 	return tableau
 
-def currentHauteur(tableau,column): # retourne la ligne (hauteur) du jeu dans une colonne
-	for i in range(LIGNES):
-		if tableau[i][column] == 0:
-			return i
-
-def isTableauCase(tableau,ligne,column):
-	return tableau[ligne][column] == 0
-
+# playerPiece is either 1 or 2
 def addPiece(tableau, ligne, column, playerPiece):
-	tableau[ligne][column] = playerPiece # 1 
+	tableau[ligne][column] = playerPiece
 
-def add(tableau, column, playerPiece):
-	ligne = currentHauteur(tableau,column)
-	if isTableauCase(tableau, ligne ,column):
-		addPiece(tableau,ligne,column, playerPiece)
+def estPosition(tableau, column):
+	# Checking if position is empty
+	return tableau[LIGNES-1][column] == 0
+	# - 1 cause array starts at 0 
+
+def getLigne(tableau, column):
+	# We check in A SPECIFIC column what's the current line
+	for l in range(LIGNES):
+		if tableau[l][column] == 0:
+			return l 
+
+def isGagnant(tableau, playerPiece):
+	for c in range(COLONNES-3): # Check horizontal
+		for l in range(LIGNES):
+			if tableau[l][c] == playerPiece and tableau[l][c+1] == playerPiece and tableau[l][c+2] == playerPiece and tableau[l][c+3] == playerPiece:
+				return True
+
+	for c in range(COLONNES): # Check vertical
+		for l in range(LIGNES-3):
+			if tableau[l][c] == playerPiece and tableau[l+1][c] == playerPiece and tableau[l+2][c] == playerPiece and tableau[l+3][c] == playerPiece:
+				return True
+
+	for c in range(COLONNES-3): # check / diagonale
+		for l in range(LIGNES-3):
+			if tableau[l][c] == playerPiece and tableau[l+1][c+1] == playerPiece and tableau[l+2][c+2] == playerPiece and tableau[l+3][c+3] == playerPiece:
+				return True
+
+	for c in range(COLONNES-3): # check \ diagonale
+		for l in range(3, LIGNES):
+			if tableau[l][c] == playerPiece and tableau[l-1][c+1] == playerPiece and tableau[l-2][c+2] == playerPiece and tableau[l-3][c+3] == playerPiece:
+				return True
 
 tableau = tableau()
-
-finJeu = False
+print(np.flip(tableau, 0))
+gameOver = False
 turn = 0
 
-while not finJeu:
+print("TAP 0 at any moment to quit")
+
+while not gameOver:
 	if turn == 0:
-		play = int(input("PLAYER 1: Please enter column: "))
-		turn += 1
-		add(tableau,play,1)
+		print(colors.bg.red, "PLAYER 1", colors.fg.lightgrey)
+		col = int(input("Player 1, make your choice: "))
+		col -= 1 # we sub 1 because user will think of first column as column 1 not column 0
+		if col == -1:
+			print(colors.reset)
+			exit()
+		if estPosition(tableau, col): # check if position is available
+			row = getLigne(tableau, col)
+			addPiece(tableau, row, col, 1)
+
+			if isGagnant(tableau, 1):
+				gameOver = True
+		turn = 1
+
+
 	else:
-		play = int(input("PLAYER 2: Please enter column: "))
+		print(colors.bg.blue, "PLAYER 2", colors.fg.lightgrey)				
+		col = int(input("Player 2, make your choice: "))
+		col -= 1
+		if col == -1:
+			print(colors.reset)
+			exit()
+		if estPosition(tableau, col):
+			row = getLigne(tableau, col)
+			addPiece(tableau, row, col, 2)
+
+			if isGagnant(tableau, 2):
+				gameOver = True
 		turn = 0
-		add(tableau,play,2)
-	print(tableau)
+
+	print(np.flip(tableau, 0))
+	print(colors.reset)
